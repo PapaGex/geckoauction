ee91ac2546ecb2d8ffb30f405058feb40b1e6987
JonathanLang <geeksgeckosandgoodvibes@gmail.com>
2023-05-01 22:26:54 -0400

    added primary routes to page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/profile";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <>
+    <Router>
     <h1 className='text-6xl font-bold underline'>Gecko Auction House</h1>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/forgot-password" element={<ForgotPassword/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/offers" element={<Offers/>} />
+    </Routes>
+    </Router>
     </>
   );
 }
---
+++ src/pages/SignOff.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignOff() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
